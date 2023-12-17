57c1673418412e6135f6a729ba7145be532ed6de
Hackerpro335 <iurewi4.tix@gmail.com>
2023-12-17 19:44:36 +0300

    добавлены фичи для страницы регистрации

--- script/regscr/regscript.js
+++ script/regscr/regscript.js
@@ -7,13 +7,12 @@ function sendForm(event){
         error.address = "слишком коротко";
         let addressField = document.getElementById("address-error");
         addressField.innerHTML = error.address;
-        addressField.previousElementSibling.classlist.add("error");
+        addressField.previousElementSibling.classList.add("error");
     }
     else{
-        error.address = null; 
         let addressField = document.getElementById("address-error");
         addressField.innerHTML = '';
-        addressField.previousElementSibling.classlist.remove("error");
+        addressField.previousElementSibling.classList.remove("error");
     }
 
     let name = event.target[0].value;
@@ -23,13 +22,12 @@ function sendForm(event){
         error.name = "введите корректное имя";
         let nameField = document.getElementById("name-error")
         nameField.innerHTML = error.name;
-        nameField.previousElementSibling.classlist.add("error");
+        nameField.previousElementSibling.classList.add("error");
     }
     else{
-        error.name = null;
         let nameField = document.getElementById("name-error");
         nameField.innerHTML = '';
-        nameField.previousElementSibling.classlist.remove("error");
+        nameField.previousElementSibling.classList.remove("error");
     }
 
     let phone = event.target[3].value;
@@ -39,14 +37,56 @@ function sendForm(event){
         error.phone = "введите правильный номер";
         let phoneField = document.getElementById("phone-error")
         phoneField.innerHTML = error.phone;
-        phoneField.previousElementSibling.classlist.add("error");
+        phoneField.previousElementSibling.classList.add("error");
     }
     else{
-        error.phone = null;
         let phoneField = document.getElementById("phone-error");
         phoneField.innerHTML = '';
-        phoneField.previousElementSibling.classlist.remove("error");
+        phoneField.previousElementSibling.classList.remove("error");
     }
 
+    let date = event.target[1].value;
+    let gender = event.target[2].value;
+    let email = event.target[3].value;
+    let url = event.target[4].value;
+    let errors = 0;
+
+    for(key in error){
+        errors++;
+    }
+
+    if(errors){
+        return false;
+    }
+    else{
+        let card = document.getElementsByClassName("card-list")[0];
+        
+        let now = new Date();
+        let birthday = new Date(date);
+        let age = now.getFullYear() - birthday.getFullYear();
+
+        card.insertAdjacentHTML("beforeend", 
+        `<div class="card">
+        <img src="img/profileIcon.png">
+        <div class="card-name">${name}</div>
+        <div class="card-age">Возраст: ${age}</div>
+        <button class="">Инфо</button>
+        </div>`)
+    }
     return false;
 }
+
+    function startDrag(event){
+        event.dataTransfer.setData("text/html", event.target.id);
+        event.dataTransfer.effectAllowed = "move";
+    }
+
+    function enterDrag(event){
+        console.log(event.target);
+        event.dataTransfer.dropEffect = "move";
+        event.target.parentElement.classList.add("active");   
+    }
+
+    function leaveDrag(event){
+        event.target.parentElement.classList.remove("active");
+    }
